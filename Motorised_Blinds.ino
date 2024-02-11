0e738d21ab5e84ef506d188d91898ce8db801c6a
aaron.p.j@hotmail.com <aaron.p.j@hotmail.com>
2024-02-11 19:39:15 +0200

    added varibales needed, need to find correct libraries

--- Motorised_Blinds.ino
+++ Motorised_Blinds.ino
@@ -1,8 +1,18 @@
-// Source code to control a DC Motor with an encoder using an ESP32
-// It will then connect to a Homebridge server to be controlled by Apple
-// HomeKit
+/*
+* Motorised Blinds - ESP32
+* Source code to control a DC Motor with an encoder using an ESP32
+* It will then connect to a Homebridge server to be controlled by
+* Apple HomeKit
+*/
 
 #include "config.h"
+#include <PIDController.h>
+
+PIDController pos_pid; 
+volatile long int encoder_pos = 0;
+unsigned int integerValue=0;  // Max value is 65535
+int motor_speed = 255;
+char incomingByte;
 
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
